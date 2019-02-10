721722f42b75dd5c03dd8e1ec356900c4332ca51
FreimanVS <freimanvs@gmail.com>
2019-02-10 12:29:06 +0300

    general->application.properties

--- src/main/java/com/vfreiman/spring/springexamples/MyAppConfiguration.java
+++ src/main/java/com/vfreiman/spring/springexamples/MyAppConfiguration.java
@@ -1,20 +1,24 @@
 package com.vfreiman.spring.springexamples;
 
 import com.vfreiman.spring.springexamples.beans.TestBean2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
-import static com.vfreiman.spring.springexamples.Constants.*;
-
 @Configuration  //The @Configuration annotation indicates to Spring that this is a configuration class
                 //that will provide beans to the Spring application context
 @ComponentScan(value={"com.vfreiman.spring.springexamples"})
 @EnableAspectJAutoProxy
+@PropertySource("classpath:application.properties")
 public class MyAppConfiguration {
+
+    @Autowired
+    private Environment environment;
 
     @Bean("testBean2")
     @Qualifier("testBean2")
@@ -27,10 +31,10 @@ public class MyAppConfiguration {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName(DRIVER);
-        driverManagerDataSource.setUrl(URL);
-        driverManagerDataSource.setUsername(USER);
-        driverManagerDataSource.setPassword(PASSWORD);
+        driverManagerDataSource.setDriverClassName(environment.getProperty("datasource.driver"));
+        driverManagerDataSource.setUrl(environment.getProperty("datasource.url"));
+        driverManagerDataSource.setUsername(environment.getProperty("datasource.user"));
+        driverManagerDataSource.setPassword(environment.getProperty("datasource.password"));
         return driverManagerDataSource;
     }
 
